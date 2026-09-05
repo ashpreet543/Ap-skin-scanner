9e77c7026a6a6ee4799c96e227cb9376e2e7892b
Parneet Khangura <preetkhangura977@gmail.com>
2026-09-05 11:04:26 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-meta = '<meta name="google-site-verification" content="jSn7w9k8ZJtsvbuiBcnyShRAADZgo2OoiPzl--mUgds" />"""
-st.markdown(meta, unsafe_allow_html=True)
+import streamlit.components.v1 as components
+components.html('<meta name="google-site-verification" content="jSn7w9k8ZJtsvbuiBcnyShRAADZgo2OoiPzl--mUgds" />', height=0, width=0)
+
 from PIL import Image
 
 st.set_page_config(page_title="HP Skin Scanner", layout="centered")
